b265975390a7f8a4692f5b270b9c3698c526629d
Nurahmat Turak <nurasoft@hotmail.com>
2020-05-13 16:16:57 +0100

    Initial and final version

---
+++ Youtube-Downloader/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace Youtube_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.Url = new System.Windows.Forms.TextBox();
+            this.Download = new System.Windows.Forms.Button();
+            this.btn_Update = new System.Windows.Forms.Button();
+            this.TSSL4 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.AutoSize = false;
+            this.statusStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 4, 2);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2,
+            this.TSSL4});
+            this.statusStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.Flow;
+            this.statusStrip1.Location = new System.Drawing.Point(0, 290);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(507, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.toolStripStatusLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel1.Margin = new System.Windows.Forms.Padding(5, 3, 5, 2);
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(148, 15);
+            this.toolStripStatusLabel1.Text = "Copy rights: BKUK Hatfield";
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.toolStripStatusLabel2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel2.Margin = new System.Windows.Forms.Padding(5, 3, 5, 2);
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(100, 15);
+            this.toolStripStatusLabel2.Text = "License:Unlicense";
+            // 
+            // Url
+            // 
+            this.Url.Location = new System.Drawing.Point(9, 17);
+            this.Url.MaxLength = 100;
+            this.Url.Name = "Url";
+            this.Url.Size = new System.Drawing.Size(301, 20);
+            this.Url.TabIndex = 1;
+            // 
+            // Download
+            // 
+            this.Download.Location = new System.Drawing.Point(316, 12);
+            this.Download.Name = "Download";
+            this.Download.Size = new System.Drawing.Size(85, 31);
+            this.Download.TabIndex = 2;
+            this.Download.Text = "Download";
+            this.Download.UseVisualStyleBackColor = true;
+            this.Download.Click += new System.EventHandler(this.Download_Click);
+            // 
+            // btn_Update
+            // 
+            this.btn_Update.Location = new System.Drawing.Point(407, 13);
+            this.btn_Update.Name = "btn_Update";
+            this.btn_Update.Size = new System.Drawing.Size(95, 30);
+            this.btn_Update.TabIndex = 4;
+            this.btn_Update.Text = "Update";
+            this.btn_Update.UseVisualStyleBackColor = true;
+            this.btn_Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // TSSL4
+            // 
+            this.TSSL4.AutoToolTip = true;
+            this.TSSL4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.TSSL4.Margin = new System.Windows.Forms.Padding(5, 3, 5, 2);
+            this.TSSL4.Name = "TSSL4";
+            this.TSSL4.Size = new System.Drawing.Size(42, 15);
+            this.TSSL4.Text = "Status:";
+            this.TSSL4.ToolTipText = "Component file status";
+            this.TSSL4.Click += new System.EventHandler(this.TSSL4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(14, 52);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(387, 224);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(507, 312);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_Update);
+            this.Controls.Add(this.Download);
+            this.Controls.Add(this.Url);
+            this.Controls.Add(this.statusStrip1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Youtube-Downloader";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.TextBox Url;
+        private System.Windows.Forms.Button Download;
+        private System.Windows.Forms.Button btn_Update;
+        private System.Windows.Forms.ToolStripStatusLabel TSSL4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Youtube-Downloader/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace Youtube_Downloader
+{
+    //delegate
+    public delegate void DelReadStdOutput(string result);
+    public delegate void DelReadErrOutput(string result);
+
+    public partial class Form1 : Form
+    {
+        //delegate event
+        public event DelReadStdOutput ReadStdOutput;
+        public event DelReadErrOutput ReadErrOutput;
+
+        String dl_1 = @"C:\youtube-dl.exe";
+        String dl_2 = Application.StartupPath + "\\youtube-dl.exe";
+
+        public Form1()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Init()
+        {
+            //3.将相应函数注册到委托事件中
+            ReadStdOutput += new DelReadStdOutput(ReadStdOutputAction);
+            ReadErrOutput += new DelReadErrOutput(ReadErrOutputAction);
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            String url = Clipboard.GetText();
+
+            if (url.Substring(0, 4) == "http")
+            {
+                Url.Text = url;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine(dl_2);
+            if (File.Exists(dl_1) && File.Exists(dl_2))
+            {
+                Console.WriteLine(dl_1.ToString());
+                Console.WriteLine(dl_2.ToString());
+                TSSL4.Text = "All exists";
+                TSSL4.BackColor = Color.Green;
+            }
+            else if (File.Exists(dl_1))
+            {
+                TSSL4.Text = "External";
+                TSSL4.BackColor = Color.Blue;
+            }
+            else if (File.Exists(dl_2))
+            {
+                TSSL4.Text = "Internal";
+                TSSL4.BackColor = Color.Blue;
+            }
+            else
+            {
+                TSSL4.Text = "Unavailable";
+                TSSL4.BackColor = Color.Gray;
+            }
+        }
+
+        private void Download_Click(object sender, EventArgs e)
+        {
+            if (TSSL4.Text != "Unavailable")
+            {
+                try
+                {
+                    richTextBox1.Clear();
+                    Process process = new Process();
+                    ProcessStartInfo startInfo = process.StartInfo;
+                    startInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+                    startInfo.FileName = "cmd.exe";
+                    startInfo.Arguments = "/c " + dl_1 + " " + Url.Text;
+                    Console.WriteLine(startInfo.Arguments.ToString());
+
+                    startInfo.UseShellExecute = false;
+                    startInfo.RedirectStandardOutput = true;
+                    startInfo.RedirectStandardInput = true;
+                    startInfo.RedirectStandardError = true;
+                    startInfo.CreateNoWindow = true;
+
+                    startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                    process.OutputDataReceived += new DataReceivedEventHandler(OnDataReceived);
+                    process.ErrorDataReceived += new DataReceivedEventHandler(OnErrorReceived);
+                    process.EnableRaisingEvents = true;
+                    process.Start();
+                    process.BeginOutputReadLine();
+                    process.BeginErrorReadLine();
+                    richTextBox1.AppendText("Downloading job is starting...\r\n");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show( ex.Message.ToString(),"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+               
+                MessageBox.Show("Component file not found, unable download.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void OnDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                // 4. 异步调用，需要invoke
+                this.Invoke(ReadStdOutput, new object[] { e.Data });
+            }
+        }
+
+        private void OnErrorReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                this.Invoke(ReadErrOutput, new object[] { e.Data });
+            }
+        }
+
+        private void ReadStdOutputAction(string result)
+        {
+            this.richTextBox1.AppendText(result + "\r\n");
+        }
+
+        private void ReadErrOutputAction(string result)
+        {
+            this.richTextBox1.AppendText(result + "\r\n");
+        }
+
+        private void CmdProcess_Exited(object sender, EventArgs e)
+        {
+            // 执行结束后触发
+        }
+
+        private void TSSL4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Showes the status of the component file(youtube-dl.exe)", "Status",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            try
+            {
+                Process process = new Process();
+                ProcessStartInfo startInfo = process.StartInfo;
+                startInfo.WindowStyle = ProcessWindowStyle.Normal;
+
+                startInfo.FileName = "cmd.exe";
+                startInfo.Arguments = "/c " + "youtube-dl.exe -U";
+                Console.WriteLine(startInfo.Arguments.ToString());
+
+                startInfo.UseShellExecute = false;
+                startInfo.RedirectStandardOutput = true;
+                startInfo.RedirectStandardInput = true;
+                startInfo.RedirectStandardError = true;
+                startInfo.CreateNoWindow = true;
+
+                startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+                process.OutputDataReceived += new DataReceivedEventHandler(OnDataReceived);
+                process.ErrorDataReceived += new DataReceivedEventHandler(OnErrorReceived);
+
+                process.EnableRaisingEvents = true;
+
+                process.Start();
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show( ex.Message.ToString(),"Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+   
+}
